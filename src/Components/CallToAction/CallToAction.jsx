cc4cac692999f4e6aaf641e50175e228d261563a
NithyashreeCS <nityaatria99@gmail.com>
2024-10-03 18:30:55 +0530

    initial changes done

--- src/Components/CallToAction/CallToAction.jsx
+++ src/Components/CallToAction/CallToAction.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
 import './CallToAction.css';
+import { useNavigate } from 'react-router-dom';
 
 const CallToAction = () => {
+    const navigate=useNavigate();
+    const handlebrowse=()=>{
+        navigate('./novels');
+    }
     return (
         <section className="cta-section">
             {/* Background video */}
@@ -15,7 +20,7 @@ const CallToAction = () => {
                 <h2>Welcome to Book Haven</h2>
                 <p>Discover thousands of books, dive into new genres, and explore new adventures.</p>
                 <div className="cta-buttons">
-                    <button className="cta-btn browse-now">Browse Now</button>
+                    <button onClick={handlebrowse} className="cta-btn browse-now">Browse Now</button>
                     <button className="cta-btn join-us">Join Us</button>
                     <button className="cta-btn explore-categories">Explore Categories</button>
                 </div>
